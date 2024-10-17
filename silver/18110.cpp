a8dbe10cba1f08350bb4724a49b137a82ea52d53
junggernaut <seungan1419@gmail.com>
2024-10-17 14:53:35 +0000

    10.17

--- silver/18110.cpp
+++ silver/18110.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
-#include <cmath>
-#include <deque>
+#include<iostream>
+#include<algorithm>
+#include<cmath>
 using namespace std;
 int main() {
 	int n;
-	deque<int> score;
 	cin >> n;
-	for (int i=0; i<n; ++i) {
-		
+	if(n==0){
+		cout << 0;
+		return 0;
 	}
+	int* scores = new int[n];
+	int cut = static_cast<int>(round(n * 0.15));
+	for(int i=0; i<n; ++i) {
+		int score;
+		cin >> score;
+		scores[i] = score;
+	}
+	sort(scores, scores+n, greater<int>());
+	double sum=0;
+	for(int i=0; i<n; ++i) {
+		if(i>=cut && i<n-cut) {
+			sum += scores[i];
+		}	
+	}
+	cout << static_cast<int>(round(sum/(n-cut*2)));
+	
+	delete[] scores;
+	return 0;
 }
